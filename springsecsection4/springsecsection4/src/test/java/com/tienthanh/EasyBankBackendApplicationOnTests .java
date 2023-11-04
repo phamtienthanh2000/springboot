2e01ccab0688cfcc42aeb258d8e554c97ee2d0bd
phamtienthanh2000 <yen17032000@gmail.com>
2023-11-04 21:07:49 +0700

    add code

---
+++ springsecsection4/springsecsection4/src/test/java/com/tienthanh/EasyBankBackendApplicationOnTests .java
@@ -0,0 +1,13 @@
+package com.tienthanh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
