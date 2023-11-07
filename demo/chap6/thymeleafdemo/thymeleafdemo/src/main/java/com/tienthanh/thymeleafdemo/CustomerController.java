ed8539a32dbe00d20f24d7c23176dcca0ad8168e
phamtienthanh2000 <yen17032000@gmail.com>
2023-11-07 18:16:02 +0700

    197
    197

---
+++ demo/chap6/thymeleafdemo/thymeleafdemo/src/main/java/com/tienthanh/thymeleafdemo/CustomerController.java
@@ -0,0 +1,37 @@
+package com.tienthanh.thymeleafdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CustomerController {
+
+    @GetMapping("/customer")
+    public String showForm(Model theModel) {
+
+        theModel.addAttribute("customer", new Customer());
+
+        return "customer-form";
+    }
+
+    @PostMapping("/customer/processForm")
+    public String processForm(
+            @Valid @ModelAttribute("customer") Customer theCustomer,
+            BindingResult theBindingResult) {
+
+        if (theBindingResult.hasErrors()) {
+            return "customer-form";
+        }
+        else {
+            return "customer-confirmation";
+        }
+    }
+}
