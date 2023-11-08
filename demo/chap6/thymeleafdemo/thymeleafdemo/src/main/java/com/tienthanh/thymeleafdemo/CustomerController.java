71e32e2ee9c0c5e3e4a8d4fafe9b8bcdcda7d1c4
phamtienthanh2000 <yen17032000@gmail.com>
2023-11-08 11:21:52 +0700

    add code chap 8
    add code chap 8

--- demo/chap6/thymeleafdemo/thymeleafdemo/src/main/java/com/tienthanh/thymeleafdemo/CustomerController.java
+++ demo/chap6/thymeleafdemo/thymeleafdemo/src/main/java/com/tienthanh/thymeleafdemo/CustomerController.java
@@ -26,6 +26,8 @@ public class CustomerController {
     public String processForm(
             @Valid @ModelAttribute("customer") Customer theCustomer,
             BindingResult theBindingResult) {
+        System.out.println("Last name: |" + theCustomer.getLastName() + "|");
+
 
         if (theBindingResult.hasErrors()) {
             return "customer-form";
